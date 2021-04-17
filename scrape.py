52b85755504712a0f134bf5adc63bb321ae7d5e1
NEO <neo.one.mail@gmail.com>
2021-04-17 08:14:19 +0200

    login

--- scrape.py
+++ scrape.py
@@ -1,5 +1,11 @@
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import os
+
 
 class FBScraper:
     def __init__(self):
@@ -17,10 +23,28 @@ class FBScraper:
         self.driver = driver
 
     def login(self):
-        pass
+        # Inputs and button
+        username_input = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name=\'email\']')))
+
+        password_input = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name=\'pass\']')))
+
+        submit_button = WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, 'button[type=\'submit\']')))
+
+        # enter the login info and login
+        username_input.clear()
+        username_input.send_keys(os.getenv('fusername'))
+
+        password_input.clear()
+        password_input.send_keys(os.getenv('password'))
+
+        submit_button.click()
 
     def download(self):
         pass
 
 
 scraper = FBScraper()
+scraper.login()
