672d72fedefd8bff0041cc5d431ad3fae62f93e6
NEO <neo.one.mail@gmail.com>
2021-04-17 19:45:17 +0200

    download images

--- scrape.py
+++ scrape.py
@@ -5,6 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import os
+import time
+import wget
+import uuid
 
 
 class FBScraper:
@@ -43,8 +46,42 @@ class FBScraper:
         submit_button.click()
 
     def download(self):
-        pass
+        loading_time = 5
+        images = []
+
+        time.sleep(loading_time)
+        self.driver.get('https://www.facebook.com/adam.cage.948/photos')
+        time.sleep(loading_time)
+
+        anchors = self.driver.find_elements_by_tag_name('a')
+        anchors = [a.get_attribute('href') for a in anchors]
+        anchors = [a for a in anchors if str(a).startswith(
+            'https://www.facebook.com/photo')]
+
+        print(f'Found {len(anchors)} links')
+
+        for i in anchors:
+            self.driver.get(i)
+            time.sleep(loading_time)
+            img = self.driver.find_elements_by_tag_name('img')
+            images.append(img[0].get_attribute('src'))
+            print(img[0].get_attribute('src'))
+
+        print(f'Found {len(images)} images')
+
+        cwd = os.getcwd()
+        path = os.path.join(cwd, 'images')
+
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+        for i in images:
+            img_path = f'{os.path.join(path, str(uuid.uuid4()))}.jpg'
+            wget.download(i, img_path)
+
+        self.driver.quit()
 
 
 scraper = FBScraper()
 scraper.login()
+scraper.download()
